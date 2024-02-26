3dab71ddccdf1ae2140be5731657b7e0d8174565
bgh <bgh2555@tukorea.ac.kr>
2024-02-27 01:16:09 +0900

    수정) url 요청에 따른 컨트롤 수정중

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, Post } from '@nestjs/common';
+import { Response } from 'express';
+import path from 'path';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    // 유저가 /login 으로 접속 했을 때 로그인 화면을 보여주기 위한 페이지
+    @Get('/login')
+    getLoginPage(@Res() res: Response) {
+        return '로그인 진입';
+        res.sendFile(path.join(__dirname,'login.html'));
+      }
+
+    // 아직 미구현
+    @Post('/signup')
+    get(){
+        return arguments;
+    }
+}
